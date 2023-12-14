718bd88ff4489bfa63a03fc44903b22fd06ee7b9
Paul Jensen <paulbjensen@gmail.com>
2023-12-14 19:35:57 +0000

    Added eslint

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+	env: {
+		es2021: true,
+		node: true,
+		mocha: true,
+	},
+	extends: 'eslint:recommended',
+	overrides: [
+		{
+			env: {
+				node: true,
+			},
+			files: ['.eslintrc.{js,cjs}'],
+			parserOptions: {
+				sourceType: 'script',
+			},
+		},
+	],
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	rules: {
+		indent: ['error', 'tab'],
+		'linebreak-style': ['error', 'unix'],
+		quotes: ['error', 'single'],
+		semi: ['error', 'always'],
+	},
+};
