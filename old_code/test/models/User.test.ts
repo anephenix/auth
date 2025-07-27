01e51ca404b19cecde377c8786f036a31a3a5c8b
Paul Jensen <paulbjensen@gmail.com>
2025-07-27 20:52:41 +0100

    Removed old code

--- old_code/test/models/User.test.ts
+++
@@ -1,7 +0,0 @@
-import { describe, it } from "vitest";
-
-describe("User model", () => {
-	it.todo("should create a user");
-	it.todo("should retrieve a user");
-	it.todo("should delete a user");
-});
