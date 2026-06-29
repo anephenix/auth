43420266b23b4e1a15b4a642f5e1554feae71455
Paul Jensen <paulbjensen@gmail.com>
2026-06-29 20:31:45 +0200

    Adjust vitest config to resolve a timeout issue with the GitHub action runner

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-	test: {
-		coverage: {
-			reporter: ["text", "json", "html"],
-		},
-	},
+  test: {
+    coverage: {
+      reporter: ["text", "json", "html"],
+    },
+    testTimeout: 10000,
+  },
 });
